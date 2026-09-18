768f192e5b73cff128eaaa86604560fa02314253
Shohjahon <russeng90@gmail.com>
2026-09-18 12:00:35 +0500

    Add Hello World program

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+
+}
+
+
+
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
